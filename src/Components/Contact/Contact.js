e1a2db35532e6838f9f983fb5508611f7f77382b
Tracy Sareto <tracysareto@gmail.com>
2024-06-20 15:14:16 +0300

    style contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -42,6 +42,26 @@ const Contact = () => {
     return (
         <div className="form-container">
             <h2 className="form-title">Contact Us</h2>
+            <p>Have a question or want to get in touch? Fill out the form </p>  
+            < div className='contact'>
+            <div className='infobox'>
+               
+                <h2>Contact Info</h2>
+                                <ul className="list-info">
+                                    <li><i className="fas fa-globe"></i> &nbsp; Mikinduri-Kunati Rd-Meru County.</li>
+                                    <li><i className="far fa-envelope"></i> &nbsp; ituuru@gmail.com</li>
+                                    <li><i className="fas fa-phone"></i>  &nbsp; +254 719 262 626 </li>
+                                    <li><i className="fas fa-phone"></i>  &nbsp; +254 789 221 911 </li>
+                                </ul>
+                                <ul className="social-icon-four">
+                                    <li className="follow">Follow on: </li>
+                                    <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                                    
+                                </ul>
+            </div>
+            <div className='formbox'>
             {status && <p className="form-status">{status}</p>}
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
@@ -64,6 +84,8 @@ const Contact = () => {
                     <button type="submit">Submit</button>
                 </div>
             </form>
+            </div>
+            </div>
         </div>
     );
 };
